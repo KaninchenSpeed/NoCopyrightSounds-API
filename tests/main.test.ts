49f97e725b554049085d6428df8a171dfe514db3
KaninchenSpeed <fhs222@outlook.de>
2022-02-20 18:45:57 +0100

    reworked tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,56 +1,15 @@
-import * as test from './modules/simple_test'
-import * as watch from './modules/watch'
-import * as output from './modules/output'
-
 import * as ncs from '../src/main'
 
-interface Test {
-  feature: Function
-  args: any[][]
-  label: string
-}
-
-const tests: Test[] = [
-  {
-    feature: ncs.getMusic,
-    args: [[undefined], [1], [2], [3]],
-    label: 'get Music'
-  },
-  {
-    feature: ncs.search,
-    args: [
-      [{ genre: 1, mood: 1 }, undefined],
-      [{ genre: 1, mood: 1 }, 1],
-      [{ genre: 4, mood: 1 }, 2],
-      [{ genre: 3, mood: 5 }, 3]
-    ],
-    label: 'search Music'
-  }
-]
-
-const run = async () => {
-  var error = false
-  for (var i = 0; i < tests.length; i++) {
-    const atest = tests[i]
-    await new Promise<void>((res, rej) => {
-      watch
-        .handel(test.promise(atest.feature, atest.args), atest.label)
-        .then(out => {
-          res()
-        })
-        .catch(err => {
-          error = true
-          res()
-        })
-    })
-  }
-  if (error) {
-    output.allFail()
-  } else {
-    output.allPass()
-  }
-}
-
-run()
-
-ncs.getMusic().then(songs => console.log(songs[0]))
+console.log('running getMusic')
+ncs.getMusic().then(songs => {
+    console.log('results getMusic')
+    console.log(songs[0])
+})
+console.log('running Search')
+ncs.search({
+    genre: 1,
+    mood: 1
+}).then(res => {
+    console.log('results Search')
+    console.log(res[0])
+})
