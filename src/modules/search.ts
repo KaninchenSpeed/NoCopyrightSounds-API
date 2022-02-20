6d0afe9a3d45e744a78099825248da89ba29e8b2
KaninchenSpeed <fhs222@outlook.de>
2022-02-20 19:51:45 +0100

    fixed search

--- src/modules/search.ts
+++ src/modules/search.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch'
 import { parse as parseHTML } from 'node-html-parser'
 import type { HTMLElement } from 'node-html-parser'
+import cheerio from 'cheerio'
 
 import Artist from '../classes/artist'
 import Song from '../classes/song'
@@ -11,43 +12,40 @@ export interface Filter {
   search?: string
 }
 
-export const search = (filter: Filter, page?: number): Promise<Song[]> => {
+export const search = (filter: Filter, page = 1): Promise<Song[]> => {
   return new Promise<Song[]>((resolve, reject) => {
-    fetch(
-      `https://ncs.io/music-search?page=${page ? page : 1}${
-        filter.genre ? `&genre=${filter.genre}` : ''
-      }${filter.mood ? `&mood=${filter.mood}` : ''}${
-        filter.search ? `&q=${filter.search}` : ''
-      }`
-    )
+    fetch(`https://ncs.io/music-search?page=${page}${filter.genre ? `&genre=${filter.genre}` : ''}${filter.mood ? `&mood=${filter.mood}` : ''}${filter.search ? `&q=${filter.search}` : ''}`)
       .then(res => res.text())
       .then(html => {
-        const root = parseHTML(html)
-        const songsHtml = root.querySelectorAll('div.col-lg-2.item')
-        const songs: Song[] = songsHtml.map((song: HTMLElement) => {
-          const link = song.querySelector('a[href]').getAttribute('href')
-          const imageUrl = song
-            .querySelector('div.img[style]')
-            .getAttribute('style')
-            ?.split("'")[1]
-          const name = song.querySelector('.bottom p strong').innerHTML
-          const optionsEl = song.querySelector('div.options')
-          const date = optionsEl.querySelector('p[title]').getAttribute('title')
-          const genre = optionsEl.querySelector('strong').innerHTML
-          const songLinkEl = optionsEl.querySelector('a.btn.black.player-play')
-          const songUrl = songLinkEl.getAttribute('data-url')
-          const artistsEl = songLinkEl.getAttribute('data-artist')
-
-          if (!artistsEl || !name || !date || !link || !imageUrl || !songUrl)
-            return new Song('', '', '', [], '', '', '')
+        const root = cheerio.load(html)
+        const table = root('.tablesorter tbody')
+        const songsHtml = root(table, 'tr').toArray()
+        const songs: Song[] = songsHtml.map(song => {
+          const [ player_col, genre_col, img_col, main_col, tags_col, date_col, tracks_col, _ ] = root('td', song).toArray()
+          
+          const link = root('a', main_col).attr('href')!
+          
+          const date_raw = root('td[style="width:15%;"]', song).filter(i => i == 1).html()!
+          const date_d = new Date(date_raw)
+          const date = `${date_d.getFullYear()}-${date_d.getMonth()}-${date_d.getDate()}`
+          const genre = root('.genre', genre_col).attr('title')!
+          
+          const player = root('.player-play', player_col)
+          const imageUrl = String(player.attr('data-cover')).replace(
+            /100x100/g,
+            '325x325'
+            )
+          const name = String(player.attr('data-track'))
+          const songUrl = String(player.attr('data-url'))
+          const artistsEl = String(player.attr('data-artist'))
+            
           const artists: Artist[] = artistsEl
             ?.split(', ')
             .map((art: string) => {
-              const artEl = parseHTML(art)
-              const url = artEl.getAttribute('href')
+              const artEl = parseHTML(art.replace(/'/g, '"')).querySelector('a')
+              const url = artEl.getAttribute('href')!
               const name = artEl.innerHTML
-              if (!url) return new Artist(name, '')
-              else return new Artist(name, url)
+              return new Artist(name, url)
             })
           return new Song(name, date, genre, artists, link, imageUrl, songUrl)
         })
