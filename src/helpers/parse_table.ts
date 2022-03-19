0ba6e322abb39a422a6bf68108be67fda386ae92
KaninchenSpeed <fhs222@outlook.de>
2022-03-19 17:08:16 +0100

    moduleised parsing tables

---
+++ src/helpers/parse_table.ts
@@ -0,0 +1,45 @@
+import { JSDOM } from 'jsdom'
+
+import Song from '../classes/song'
+import Artist from '../classes/artist'
+
+export default (table: Element) => {
+  const rows = table.querySelectorAll('tr')
+  const songs = Array.from(rows).map(el => {
+    const [
+      player_col,
+      genre_col,
+      img_col,
+      main_col,
+      tags_col,
+      date_col,
+      tracks_col,
+      _
+    ] = Array.from(el.querySelectorAll('td'))
+
+    const url = main_col.querySelector('a')!.href
+    const date_obj = new Date(
+      el.querySelectorAll('td[style="width:15%;"]')[1].innerHTML
+    )
+    const date = `${date_obj.getFullYear()}-${date_obj.getMonth()}-${date_obj.getDate()}`
+    const genre = genre_col.querySelector('.genre')!.getAttribute('title')!
+
+    const player = player_col.querySelector('.player-play')!
+    const cover = player
+      .getAttribute('data-cover')!
+      .replace(/100x100/g, '325x325')
+    const name = player.getAttribute('data-track')!
+    const song_url = player.getAttribute('data-url')!
+
+    const artists_el = player.getAttribute('data-artist')!
+    const artists: Artist[] = artists_el.split(', ').map(art => {
+      const art_el = new JSDOM(art).window.document.querySelector('a')!
+      const url = art_el.getAttribute('href')!
+      const name = art_el.innerHTML
+      return new Artist(name, url)
+    })
+
+    return new Song(name, date, genre, artists, url, cover, song_url)
+  })
+  return songs
+}
--- src/modules/search.ts
+++ src/modules/search.ts
@@ -1,7 +1,7 @@
 import { JSDOM } from 'jsdom'
 import axios from 'axios'
+import parse_table from '../helpers/parse_table'
 
-import Artist from '../classes/artist'
 import Song from '../classes/song'
 
 export interface Filter {
@@ -24,42 +24,6 @@ export const search = async (filter: Filter, page = 1): Promise<Song[]> => {
   const dom = new JSDOM(html)
   const document = dom.window.document
   const table = document.querySelector('.tablesorter tbody')!
-  const song_els = table.querySelectorAll('tr')
-  const songs: Song[] = Array.from(song_els).map(el => {
-    const [
-      player_col,
-      genre_col,
-      img_col,
-      main_col,
-      tags_col,
-      date_col,
-      tracks_col,
-      _
-    ] = Array.from(el.querySelectorAll('td'))
-
-    const url = main_col.querySelector('a')!.href
-    const date_obj = new Date(
-      el.querySelectorAll('td[style="width:15%;"]')[1].innerHTML
-    )
-    const date = `${date_obj.getFullYear()}-${date_obj.getMonth()}-${date_obj.getDate()}`
-    const genre = genre_col.querySelector('.genre')!.getAttribute('title')!
-
-    const player = player_col.querySelector('.player-play')!
-    const cover = player
-      .getAttribute('data-cover')!
-      .replace(/100x100/g, '325x325')
-    const name = player.getAttribute('data-track')!
-    const song_url = player.getAttribute('data-url')!
-
-    const artists_el = player.getAttribute('data-artist')!
-    const artists: Artist[] = artists_el.split(', ').map(art => {
-      const art_el = new JSDOM(art).window.document.querySelector('a')!
-      const url = art_el.getAttribute('href')!
-      const name = art_el.innerHTML
-      return new Artist(name, url)
-    })
-
-    return new Song(name, date, genre, artists, url, cover, song_url)
-  })
+  const songs = parse_table(table)
   return songs
 }
