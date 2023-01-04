33d656b71725089290228731b321b357c0f60a34
KaninchenSpeed <fhs222@outlook.de>
2023-01-04 15:05:16 +0100

    better loging

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,20 +2,20 @@ import * as ncs from '../src/main'
 
 console.log('running getMusic')
 ncs.getMusic().then((res) => {
-    console.log('results getMusic')
-    console.log(res[0], res.length)
+    console.log('results getMusic', res.length)
+    console.dir(res[0], { depth: null })
 })
 
 console.log('running Search')
 ncs.search({
     search: 'you'
 }).then((res) => {
-    console.log('results Search')
-    console.log(res[0], res.length, res[0].tags?.at(0), res[0].tags?.length)
+    console.log('results Search', res.length)
+    console.dir(res[0], { depth: null })
 })
 
 console.log('running artist info')
 ncs.getArtistInfo('/artist/172/harley-bird').then((res) => {
     console.log('results artist info')
-    console.log({ ...res, songs: [res.songs[0]] })
+    console.dir({ ...res, songs: [res.songs[0]] }, { depth: null })
 })
