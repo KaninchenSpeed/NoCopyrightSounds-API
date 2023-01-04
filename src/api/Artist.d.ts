59dc3828f2033d9e9b505bbe82fbb55e4213f700
KaninchenSpeed <fhs222@outlook.de>
2023-01-04 14:11:35 +0100

    adding more info to song

--- src/api/Artist.d.ts
+++ src/api/Artist.d.ts
@@ -1,4 +1,4 @@
 export default interface Artist {
-  name: string
-  url: string
+    name: string
+    url: string
 }
--- src/api/Song.d.ts
+++ src/api/Song.d.ts
@@ -2,12 +2,17 @@ import type Artist from './Artist'
 import type Tag from './Tag'
 
 export default interface Song {
-  name: string
-  date: string
-  genre: string
-  artists: Artist[]
-  url: string
-  imageUrl: string
-  songUrl: string
-  tags?: Tag[]
+    name: string
+    date: Date
+    genre?: string
+    artists: Artist[]
+    url: string
+    coverUrl?: string
+    previewUrl?: string
+    id: string
+    download: {
+        regular?: string
+        instrumental?: string
+    }
+    tags: Tag[]
 }
