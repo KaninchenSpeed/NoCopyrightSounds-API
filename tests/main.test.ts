51fc86af328ce3a4fd5add0d5d4e197ea92b5ee5
KaninchenSpeed <fhs222@outlook.de>
2023-01-04 14:43:22 +0100

    removing Client class tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -19,30 +19,3 @@ ncs.getArtistInfo('/artist/172/harley-bird').then((res) => {
     console.log('results artist info')
     console.log({ ...res, songs: [res.songs[0]] })
 })
-
-console.log('running client tests')
-const client = new ncs.Client({
-    detailed_log: true
-})
-
-console.log('running client getMusic')
-client.getSongs().then((res) => {
-    console.log('results client getMusic')
-    console.log(res[0], res.length)
-})
-
-console.log('running client Search')
-client
-    .search({
-        search: 'you'
-    })
-    .then((res) => {
-        console.log('results client Search')
-        console.log(res[0], res.length, res[0].tags?.at(0), res[0].tags?.length)
-    })
-
-console.log('running client artist info')
-client.getArtistInfo('/artist/172/harley-bird').then((res) => {
-    console.log('results client artist info')
-    console.log({ ...res, songs: [res.songs[0]] })
-})
