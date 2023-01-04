6bb66e977031cca1512b04bfdfdd172b0232741c
KaninchenSpeed <fhs222@outlook.de>
2023-01-04 14:43:08 +0100

    fixing artist image

--- src/modules/artist.ts
+++ src/modules/artist.ts
@@ -6,38 +6,31 @@ import type Artist from '../api/Artist'
 import type Song from '../api/Song'
 import type Artist_info from '../api/ArtistInfo'
 
-export const getArtistInfo = async (artist_url: string | Artist): Promise<Artist_info> => {
-    if (typeof artist_url != 'string') artist_url = artist_url.url
-    const { data: html } = await axios.get<string>(`https://ncs.io${artist_url}`, {
+export const getArtistInfo = async (artistUrl: string | Artist): Promise<Artist_info> => {
+    if (typeof artistUrl != 'string') artistUrl = artistUrl.url
+    const { data: html } = await axios.get<string>(`https://ncs.io${artistUrl}`, {
         responseType: 'text'
     })
 
     const dom = new JSDOM(html)
     const document = dom.window.document.body
 
-    const info_el = document.querySelector('.details .info')!
-    const name = info_el.querySelector('h5')!.innerHTML
-    const genres = info_el.querySelector('.tags')!.innerHTML.split(', ')
-    const img = info_el.querySelector<HTMLDivElement>('.img')
-        ? info_el
-              .querySelector<HTMLDivElement>('.img')!
-              .getAttribute('style')!
-              .trim()
-              .replace("url('", '')
-              .replace("')", '')
-        : undefined
+    const infoEl = document.querySelector('.details .info')!
+    const name = infoEl.querySelector('h5')!.innerHTML
+    const genres = infoEl.querySelector('.tags')!.innerHTML.split(', ')
+    const img = document.querySelector<HTMLDivElement>('.img')?.getAttribute('style')?.trim()?.replace("background-image: url('", '')?.replace("')", '')
 
-    const fearured_el = document.querySelector('.featured tbody')
-    const featured: Song[] = !fearured_el ? [] : parse_table(fearured_el)
+    const fearuredEl = document.querySelector('.featured tbody')
+    const featured: Song[] = !fearuredEl ? [] : parse_table(fearuredEl)
 
-    const songs_el = fearured_el
+    const songsEl = fearuredEl
         ? document.querySelectorAll('.table tbody')[1]
         : document.querySelector('.table tbody')!
-    const songs: Song[] = parse_table(songs_el)
+    const songs: Song[] = parse_table(songsEl)
 
     return {
         name,
-        url: artist_url,
+        url: artistUrl,
         img,
         genres,
         featured,
