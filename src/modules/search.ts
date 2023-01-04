e4be358835b86d26363a50f78d2cf975be03fa4e
KaninchenSpeed <fhs222@outlook.de>
2023-01-04 14:10:24 +0100

    reformating

--- src/modules/search.ts
+++ src/modules/search.ts
@@ -1,29 +1,29 @@
 import { JSDOM } from 'jsdom'
 import axios from 'axios'
-import parse_table from '../helpers/parse_table'
+import parse_table from '../helpers/parseTable'
 
 import type Song from '../api/Song'
 
 export interface Filter {
-  genre?: number
-  mood?: number
-  search?: string
+    genre?: number
+    mood?: number
+    search?: string
 }
 
 export const search = async (filter: Filter, page = 1): Promise<Song[]> => {
-  const { data: html } = await axios.get<string>(
-    `https://ncs.io/music-search?page=${page}${
-      filter.genre ? `&genre=${filter.genre}` : ''
-    }${filter.mood ? `&mood=${filter.mood}` : ''}${
-      filter.search ? `&q=${filter.search}` : ''
-    }`,
-    {
-      responseType: 'text'
-    }
-  )
-  const dom = new JSDOM(html)
-  const document = dom.window.document
-  const table = document.querySelector('.tablesorter tbody')!
-  const songs = parse_table(table)
-  return songs
+    const { data: html } = await axios.get<string>(
+        `https://ncs.io/music-search?page=${page}${
+            filter.genre ? `&genre=${filter.genre}` : ''
+        }${filter.mood ? `&mood=${filter.mood}` : ''}${
+            filter.search ? `&q=${filter.search}` : ''
+        }`,
+        {
+            responseType: 'text'
+        }
+    )
+    const dom = new JSDOM(html)
+    const document = dom.window.document
+    const table = document.querySelector('.tablesorter tbody')!
+    const songs = parse_table(table)
+    return songs
 }
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,49 +2,47 @@ import * as ncs from '../src/main'
 
 console.log('running getMusic')
 ncs.getMusic().then(res => {
-  console.log('results getMusic')
-  console.log(res[0], res.length)
+    console.log('results getMusic')
+    console.log(res[0], res.length)
 })
 
 console.log('running Search')
-ncs
-  .search({
+ncs.search({
     search: 'you'
-  })
-  .then(res => {
+}).then(res => {
     console.log('results Search')
     console.log(res[0], res.length, res[0].tags?.at(0), res[0].tags?.length)
-  })
+})
 
 console.log('running artist info')
 ncs.getArtistInfo('/artist/172/harley-bird').then(res => {
-  console.log('results artist info')
-  console.log({ ...res, songs: [res.songs[0]] })
+    console.log('results artist info')
+    console.log({ ...res, songs: [res.songs[0]] })
 })
 
 console.log('running client tests')
 const client = new ncs.Client({
-  detailed_log: true
+    detailed_log: true
 })
 
 console.log('running client getMusic')
 client.getSongs().then(res => {
-  console.log('results client getMusic')
-  console.log(res[0], res.length)
+    console.log('results client getMusic')
+    console.log(res[0], res.length)
 })
 
 console.log('running client Search')
 client
-  .search({
-    search: 'you'
-  })
-  .then(res => {
-    console.log('results client Search')
-    console.log(res[0], res.length, res[0].tags?.at(0), res[0].tags?.length)
-  })
+    .search({
+        search: 'you'
+    })
+    .then(res => {
+        console.log('results client Search')
+        console.log(res[0], res.length, res[0].tags?.at(0), res[0].tags?.length)
+    })
 
 console.log('running client artist info')
 client.getArtistInfo('/artist/172/harley-bird').then(res => {
-  console.log('results client artist info')
-  console.log({ ...res, songs: [res.songs[0]] })
+    console.log('results client artist info')
+    console.log({ ...res, songs: [res.songs[0]] })
 })
